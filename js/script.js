41e2fae2e1a9168bea840bbfaa86b0ee85012b4c
Ruma <matteoh8@hotmail.it>
2022-10-19 15:24:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// chiedere nome
+// stampare risultato
+// chiedere cognome
+// stampare risultato
+// chiedere colore prefe
+// stampare risultato
+// scrivi nomecognomecolorepreferito22
+
+let name = prompt('Qual è il tuo nome?');
+document.getElementById('name');
+console.log(name);
+
+let last_name = prompt('Qual è il tuo cognome?');
+document.getElementById('last_name');
+console.log(last_name);
+
+let color = prompt('Qual è il tuo colore preferito?');
+document.getElementById('color');
+console.log('color');
+
+console.log (
+    `
+    ${name}${last_name}${color}22;
+    `
+);
